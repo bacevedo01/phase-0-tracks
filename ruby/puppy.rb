2d4cba6067c865b909942aa94aad27fcfaf4d21a
bacevedo01 <bacevedo@gmail.com>
2016-11-13 20:57:51 -0800

    Add 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+#SPECIES ------------------------
+#Puppies
+
+#CHARACTERISTICS ----------------
+#Hair length: varies
+#Kid Friendly: yes
+#Coat type: wired, short, long
+#Name: varies
+#Playful: yes
+
+#BEHAVIOR -----------------------
+#Jump
+#Hug
+#Scamper
+#Chewing Objects
+#Barking
