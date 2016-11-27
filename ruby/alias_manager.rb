b78c129244e8582f746f2eabe48871fdcb02c260
bacevedo01 <bacevedo@gmail.com>
2016-11-27 12:35:40 -0800

    Add storing names in hash and recalling.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -29,29 +29,51 @@ alphabet = { "a" => "e", "b" => "c", "c" => "d", "d" => "f", "e" => "i", "f" => 
 # p alphabet["e"]
 output = ""
 output2 = ""
+new_aliases = {}
+real_name = ""
+loop do 
+	puts "Alias Manager: Enter as many aliases as you want."
+	puts "Type [done] for your first name when finished."
+	puts "Enter your first name."
+		first_name = gets.chomp.downcase
 
-puts "Alias Manager"
-puts "Enter your first name."
-	first_name = gets.chomp.downcase
+	if first_name == "done"
+		break
+	end
+
+	puts "Enter your last name."
+		last_name = gets.chomp.downcase
+	
 	new_last_name = first_name.split("")
-p new_last_name
-puts "Enter your last name."
-	last_name = gets.chomp.downcase
 	new_first_name = last_name.split("")
-p new_first_name
 
-new_last_name.each do |x|
-	output << alphabet[x]
-	output[0] = output[0].capitalize
+	new_last_name.each do |x|
+		output << alphabet[x]
+		output[0] = output[0].capitalize
+	end
+
+	new_first_name.each do |x|
+		output2 << alphabet[x]
+		output2[0] = output2[0].capitalize
+	end
+	
+	first_name[0] = first_name[0].capitalize
+	last_name[0] = last_name[0].capitalize
+	real_name = first_name + last_name
+	real_name = real_name.gsub(/(?<=[a-z])(?=[A-Z])/, ' ')
+
+	new_alias = output2 + output
+	new_alias = new_alias.gsub(/(?<=[a-z])(?=[A-Z])/, ' ')
+
+	new_aliases[real_name] = new_alias
+
+	puts "Your new name is #{new_alias}!"
 end
 
-new_first_name.each do |x|
-	output2 << alphabet[x]
-	output2[0] = output2[0].capitalize
+
+new_aliases.each do |real_name, new_alias| 
+	puts "#{real_name} is #{new_alias}."
 end
-
-puts "Your new name is #{output2} #{output}!"
-
 
 
 
