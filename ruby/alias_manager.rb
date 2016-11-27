f5ba3d6ff474bd2d579e2d4cf590edbef71977cc
bacevedo01 <bacevedo@gmail.com>
2016-11-27 06:54:27 -0800

    Add test for input.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -24,3 +24,10 @@ alphabet = { "a" => "e", "b" => "c", "c" => "d", "d" => "f", "e" => "i", "f" => 
 			}
 
 p alphabet
+p alphabet["a"]
+p alphabet["b"]
+p alphabet["e"]
+
+puts "Enter a letter."
+first_name = gets.chomp
+puts "#{alphabet[first_name]} is the answer"
