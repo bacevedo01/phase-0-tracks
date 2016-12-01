62e513bc2b104424611e9ec48eb2bf42466e6a3f
bacevedo01 <bacevedo@gmail.com>
2016-12-01 14:31:25 -0800

    Add random santa generator code.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -3,11 +3,11 @@ class Santa
 	attr_reader :age, :ethnicity, :ranking
 	attr_accessor :gender
 
-	def initialize(gender, ethnicity)
+	def initialize(gender, ethnicity, age)
 		puts "Initializing Santa instance ..."
 		@gender = gender
 		@ethnicity = ethnicity
-		@age = 0
+		@age = age
 		@ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 	end
 
@@ -32,50 +32,54 @@ class Santa
 		p @ranking
 	end
 
-	# def change_gender(new_gender)
-	# 	@gender = new_gender
-	# end
+								# def change_gender(new_gender)
+								# 	@gender = new_gender
+								# end
 
-	# def age
-	# 	@age
-	# end
+								# def age
+								# 	@age
+								# end
 
-	# def ethnicity
-	# 	@ethnicity
-	# end
+								# def ethnicity
+								# 	@ethnicity
+								# end
 
-	# def ranking
-	# 	@ranking
-	# end
+								# def ranking
+								# 	@ranking
+								# end
 
 end
 
-# santas = []
-# santas << Santa.new("agender", "black")
-# santas << Santa.new("female", "Latino")
-# santas << Santa.new("bigender", "white")
-# santas << Santa.new("male", "Japanese")
-# santas << Santa.new("female", "prefer not to say")
-# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-# santas << Santa.new("N/A", "N/A")
+								# santas = []
+								# santas << Santa.new("agender", "black")
+								# santas << Santa.new("female", "Latino")
+								# santas << Santa.new("bigender", "white")
+								# santas << Santa.new("male", "Japanese")
+								# santas << Santa.new("female", "prefer not to say")
+								# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+								# santas << Santa.new("N/A", "N/A")
+								#santa = Santa.new("male", "white", 35)
+								# # santa.speak
+								# # santa.eat_milk_and_cookies("Peanut Butter")
 
-santa = Santa.new("male", "white")
-# # santa.speak
-# # santa.eat_milk_and_cookies("Peanut Butter")
+								# santas.each do |x|
+								# 	x.speak
+								# 	x.eat_milk_and_cookies("chocolate chip")
+								# 	puts "This Santa is #{x.age} years old!"
+								# end
 
-# santas.each do |x|
-# 	x.speak
-# 	x.eat_milk_and_cookies("chocolate chip")
-# 	puts "This Santa is #{x.age} years old!"
-# end
+								#santa.get_made_at("Rudolph")
+								#santa.get_made_at("Donner")
+								#santa.celebrate_birthday
+genders = ["male", "female", "gender fluid", "bigender"]
+ethnicities = ["white", "black", "asian", "native american", "hispanic", "N/A"]
 
-santa.get_made_at("Rudolph")
-santa.get_made_at("Donner")
-#santa.celebrate_birthday
+santa = Santa.new(genders.shuffle.sample, ethnicities.shuffle.sample, rand(140))
+
+p santa.gender
 p santa.age
 p santa.ethnicity
-santa.gender = "female"
-p santa.gender
+
 
 
 
