b65ceab92b1d9eee5b577771298e33b0b233a101
bacevedo01 <bacevedo@gmail.com>
2016-11-30 18:56:27 -0800

    Add ranking method.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -4,7 +4,8 @@ class Santa
 		puts "Initializing Santa instance ..."
 		@gender = gender
 		@ethnicity = ethnicity
-		
+		@age = 0
+		@ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 	end
 
 	def speak
@@ -17,13 +18,25 @@ class Santa
 		puts "That was a good #{cookie} cookie!" 
 	end
 
-	def reindeer_ranking(ranking)
-		@ranking = ranking
-		ranking  = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	def celebrate_birthday
+		@age += 1
 	end
 
-	def santa_age(age)
-		@age = 0
+	def get_made_at (reindeer)
+		n = @ranking.index(reindeer)
+		@ranking.insert(-1, @ranking.delete_at(n))
+	end
+
+	def change_gender(new_gender)
+		@gender = new_gender
+	end
+
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
 	end
 end
 
@@ -43,4 +56,8 @@ santas << Santa.new("N/A", "N/A")
 santas.each do |x|
 	x.speak
 	x.eat_milk_and_cookies("chocolate chip")
+	puts "This Santa is #{x.age} years old!"
 end
+
+
+
