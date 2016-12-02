98bc8458b6f45f0e3796ee0cea59fb24e67d5a5a
bacevedo01 <bacevedo@gmail.com>
2016-12-01 17:02:55 -0800

    Release 2 update.

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -18,45 +18,64 @@
 # Create list method
 
 
-	def create_list(grocery_string)
-		grocery_list = {}
-		grocery_array = grocery_string.split(" ")
-		grocery_array.each do |item|
-			grocery_list[item] = 0
-		end
-		return grocery_list
+def create_list(grocery_string)
+	grocery_list = {}
+	grocery_array = grocery_string.split(",")
+	grocery_array.each do |item|
+		grocery_list[item] = 0
 	end
+	return grocery_list
+end
  	
- 	temp = create_list("apples pears pizza juice")
+temp = create_list("")
 
 # Method to add an item and optional quanity to a list
-	def add_item(grocery_list, food_item, quantity = 0)
-		grocery_list[food_item] = quantity
-		return grocery_list
-	end
+def add_item(grocery_list, food_item, quantity = 0)
+	grocery_list[food_item] = quantity
+	#return grocery_list
+end
 
-p add_item(temp,"burger")
+#add_item(temp,"burger")
+#p temp
 
+def remove_item(grocery_list,food_item)
+	grocery_list.delete(food_item)
+	#return grocery_list
+end
 
-	def remove_item(grocery_list,food_item)
-		grocery_list.delete(food_item)
-		return grocery_list
-	end
-p remove_item(temp, "pizza")
+#remove_item(temp, "pizza")
+#p temp 
 
+def update_item(grocery_list, food_item, quantity)
+	grocery_list[food_item] = quantity
+	#return grocery_list
+end
 
+#update_item(temp, "pears", 300)
+# p temp 
 
-# Method to remove an item from the list
-# input:
-# steps:
-# output:
-
-# Method to update the quantity of an item
-# input:
-# steps:
-# output:
 
 # Method to print a list and make it look pretty
+def print_list(grocery_list)
+		puts "Here is your grocery list:"
+
+		grocery_list.each do |food_item, quantity|
+			puts "You need #{quantity} of #{food_item}"
+		end
+end
+
+add_item(temp, "Lemonade", 2)
+add_item(temp, "Tomatoes", 3)
+add_item(temp, "Onions", 1)
+add_item(temp, "Ice Cream", 4)
+
+print_list(temp)
+
+remove_item(temp, "Lemonade")
+update_item(temp, "Ice Cream", 1)
+
+print_list(temp)
+
 # input:
 # steps:
 # output:
