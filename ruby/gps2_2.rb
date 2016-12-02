896b0d0bdd3a325c61cc98f3828f8f85a9742f24
bacevedo01 <bacevedo@gmail.com>
2016-12-01 16:12:59 -0800

    Add pseudocode and initial list creation.

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,50 @@
+#Grocery List
+#Create a new list
+#Add an item to list
+#Remove an item to list
+#Update qualities
+#Print the list
+
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # input
+  # .split the string into a hash with a default quanity
+  # set default quantity
+  # print the list to the console and make it look nice.
+# output: pring out a hash
+
+
+
+	def create_list(grocery_string)
+		grocery_list = {}
+		grocery_array = grocery_string.split(" ")
+		grocery_array.each do |hash|
+			grocery_list[hash] = 0
+		end
+		return grocery_list
+	end
+
+p create_list("apples pears pizza juice")
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps:
+# output:
+
+# Method to remove an item from the list
+# input:
+# steps:
+# output:
+
+# Method to update the quantity of an item
+# input:
+# steps:
+# output:
+
+# Method to print a list and make it look pretty
+# input:
+# steps:
+# output:
+
+
