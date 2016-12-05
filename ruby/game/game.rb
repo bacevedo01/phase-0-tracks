65a0c4a6bbe3fca9a44d5e4c5af73ce871f613d8
bacevedo01 <bacevedo@gmail.com>
2016-12-04 21:28:04 -0800

    Add file structure and pseudocode.

---
+++ ruby/game/game.rb
@@ -0,0 +1,8 @@
+#Hangman game
+#Player 1 can enter a word and it is stored
+#Player 2 can guess the word and has as many guesses as letters in the word.
+#Player can recieve feedback for their guess. 
+#Player can recieve postive feedback for a good guess and does not lose a guess.
+#Player can recieve negative feedback for a wrong guess and loses a guess.
+#Player can recieve a congratulatory message for correctly guessing the word.
+#Player can recieve a negative message for running out of guesses.
