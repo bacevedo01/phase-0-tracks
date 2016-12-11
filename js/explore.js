6d1ddbf6e165d5cecd010527a3e027c217e23150
bacevedo01 <bacevedo@gmail.com>
2016-12-11 15:15:51 -0800

    Add pseudocode for code.

---
+++ js/explore.js
@@ -0,0 +1,4 @@
+// Take the input string or argument and place it in a variable.
+// Create a loop (using a countdown loop) that starts at the ending character of the variable
+// and then puts that character and adds it to a string. 
+
