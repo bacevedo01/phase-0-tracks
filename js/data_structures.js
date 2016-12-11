50c58b68040a303a33401aa63945ab1ddc572a13
bacevedo01 <bacevedo@gmail.com>
2016-12-11 15:47:05 -0800

    Add combining array function to code.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,10 +1,25 @@
 
 
 var colors = ["Blue","Red", "Green", "Yellow"]
-var horses = ["Ed", "Silver", "Pegesus", "Comet"]
+var horse_names = ["Ed", "Silver", "Pegesus", "Comet"]
 
 colors.push("White")
-horses.push("Horsey")
+horse_names.push("Horsey")
 
 console.log(colors)
+console.log(horse_names)
+
+function combine_arrays( keys, values)
+{
+    var horses = {};
+    if ( keys.length != values.length)
+       return null;
+    for (var index in keys)
+        horses[keys[index]] = values[index];
+     return horses;
+};
+
+
+var horses = combine_arrays( horse_names, colors);
+
 console.log(horses)
