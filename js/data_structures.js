5c6a82048b53c017af23abaa4672a483796da024
bacevedo01 <bacevedo@gmail.com>
2016-12-11 15:34:53 -0800

    Added code to amend the arrays.

---
+++ js/data_structures.js
@@ -0,0 +1,10 @@
+
+
+var colors = ["Blue","Red", "Green", "Yellow"]
+var horses = ["Ed", "Silver", "Pegesus", "Comet"]
+
+colors.push("White")
+horses.push("Horsey")
+
+console.log(colors)
+console.log(horses)
