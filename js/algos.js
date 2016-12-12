cd99d2009b8d97a59518189a0d65279b588d1058
bacevedo01 <bacevedo@gmail.com>
2016-12-11 16:43:07 -0800

    Add functionality for input arguments from any array as a function.

--- js/algos.js
+++ js/algos.js
@@ -6,17 +6,23 @@
 // Find the length of each element in the array.
 // Sort the array and return the longest element (which should be in the 0 position)
 
-var words = ['poop', 'poopy', 
-           'poo'];
 
+function longest_element(input_array) {
 var length = 0;
 var longest;
 
-for(var i=0; i < words.length; i++){
-    if(words[i].length > length){
-        var length = words[i].length;
-        longest = words[i];
+for(var i=0; i < input_array.length; i++){
+    if(input_array[i].length > length){
+        var length = input_array[i].length;
+        longest = input_array[i];
     }      
-} 
+	} 
+	console.log(longest);
+}
 
-console.log(longest);
+var words = ['poop', 'poopy', 'poo'];
+var words2 = ['dog', 'elephant', 'bear'];
+
+longest_element(words)
+longest_element(words2)
+longest_element(["run","runs","running"])
