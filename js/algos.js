6cfeb14f4bbfbf02729d8a8eee88e1a9205e95e4
bacevedo01 <bacevedo@gmail.com>
2016-12-11 16:35:29 -0800

    Add solution for a single array to find the longest element.

--- js/algos.js
+++ js/algos.js
@@ -5,3 +5,18 @@
 // Loop through each element of array
 // Find the length of each element in the array.
 // Sort the array and return the longest element (which should be in the 0 position)
+
+var words = ['poop', 'poopy', 
+           'poo'];
+
+var length = 0;
+var longest;
+
+for(var i=0; i < words.length; i++){
+    if(words[i].length > length){
+        var length = words[i].length;
+        longest = words[i];
+    }      
+} 
+
+console.log(longest);
