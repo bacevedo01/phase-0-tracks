8f8252900906bce90cf5e538da176bc7588a89a5
bacevedo01 <bacevedo@gmail.com>
2016-12-11 16:30:24 -0800

    Add inital pseudocode.

---
+++ js/algos.js
@@ -0,0 +1,7 @@
+
+// GOAL: write a function that takes an array of words or phrases and returns the longest word or phrase in the array.
+//
+// Pseudocode:
+// Loop through each element of array
+// Find the length of each element in the array.
+// Sort the array and return the longest element (which should be in the 0 position)
