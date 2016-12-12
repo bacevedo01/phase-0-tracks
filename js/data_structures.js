9c3da304b18dcd69f4af7cd285d8699050d00515
bacevedo01 <bacevedo@gmail.com>
2016-12-11 16:02:50 -0800

    Add Car creator function and driver code for 3 cars.

--- js/data_structures.js
+++ js/data_structures.js
@@ -23,3 +23,32 @@ function combine_arrays( keys, values)
 var horses = combine_arrays( horse_names, colors);
 
 console.log(horses)
+
+function Car(make, model, color) {
+	this.make = make;
+	this.model = model;
+	this.color = color;
+
+	this.vroom = function() { console.log("Vrrooom!"); };
+
+	console.log("-----------");
+}
+
+var newCar = new Car("Dodge", "Challenger", "Black");
+console.log(newCar.make);
+console.log(newCar.model);
+console.log(newCar.color);
+newCar.vroom();
+
+var newCar = new Car("Porche", "911 GT", "White w/ stripes");
+console.log(newCar.make);
+console.log(newCar.model);
+console.log(newCar.color);
+newCar.vroom();
+
+var newCar = new Car("Ford", "GT", "Matte black");
+console.log(newCar.make);
+console.log(newCar.model);
+console.log(newCar.color);
+newCar.vroom();
+
